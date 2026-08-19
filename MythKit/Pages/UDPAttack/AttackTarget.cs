9bcb0c9e4cbfdb8f27bc9b2fda1c954d13333842
CreeperMPG <mpgifn@163.com>
2026-08-19 19:53:49 +0800

    更改项目名称为 MythKit

---
+++ MythKit/Pages/UDPAttack/AttackTarget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MythKit.Pages.UDPAttack
+{
+    public enum AttackTarget
+    {
+        Student = 1,
+        Teacher = 2,
+        All = 3
+    }
+}
