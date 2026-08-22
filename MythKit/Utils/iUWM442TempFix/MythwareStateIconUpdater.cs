36fcbc92e1b8e6258475acc42563de9e6c1848ac
CreeperMPG <mpgifn@163.com>
2026-08-22 21:11:52 +0800

    图标更新临时修复 => iUWM #442

---
+++ MythKit/Utils/iUWM442TempFix/MythwareStateIconUpdater.cs
@@ -0,0 +1,48 @@
+﻿using iNKORE.UI.WPF.Modern.Common.IconKeys;
+using iNKORE.UI.WPF.Modern.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MythKit.Mythware;
+
+namespace MythKit.Utils.iUWM442TempFix
+{
+    internal class MythwareStateIconUpdater
+    {
+        public static readonly DependencyProperty StateProperty =
+            DependencyProperty.RegisterAttached(
+                "State",
+                typeof(MythwareState),
+                typeof(MythwareStateIconUpdater),
+                new PropertyMetadata(MythwareState.Invalid, OnStateChanged));
+
+        public static void SetState(DependencyObject element, MythwareState value)
+            => element.SetValue(StateProperty, value);
+
+        public static MythwareState GetState(DependencyObject element)
+            => (MythwareState)element.GetValue(StateProperty);
+
+        private static void OnStateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is FontIcon icon))
+                return;
+
+            var state = (MythwareState)e.NewValue;
+            switch (state)
+            {
+                case MythwareState.Running:
+                    icon.Icon = FluentSystemIcons.CheckboxChecked_16_Filled;
+                    break;
+                case MythwareState.NotRunning:
+                    icon.Icon = FluentSystemIcons.List_16_Filled;
+                    break;
+                case MythwareState.Invalid:
+                    icon.Icon = FluentSystemIcons.Question_16_Filled;
+                    break;
+            }
+        }
+    }
+}
---
+++ MythKit/Utils/iUWM442TempFix/TaskManagerIconUpdater.cs
@@ -0,0 +1,55 @@
+﻿using iNKORE.UI.WPF.Modern.Common.IconKeys;
+using iNKORE.UI.WPF.Modern.Controls;
+using MythKit.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MythKit.Utils.iUWM442TempFix
+{
+    internal class TaskManagerIconUpdater
+    {
+        public static readonly DependencyProperty StateProperty =
+            DependencyProperty.RegisterAttached(
+                "State",
+                typeof(TaskState),
+                typeof(TaskManagerIconUpdater),
+                new PropertyMetadata(TaskState.Idle, OnStateChanged));
+
+        public static void SetState(DependencyObject element, TaskState value)
+            => element.SetValue(StateProperty, value);
+
+        public static TaskState GetState(DependencyObject element)
+            => (TaskState)element.GetValue(StateProperty);
+
+        private static void OnStateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is FontIcon icon))
+                return;
+
+            var state = (TaskState)e.NewValue;
+            switch (state)
+            {
+                case TaskState.Idle:
+                    icon.Icon = FluentSystemIcons.Flag_20_Regular;
+                    break;
+                case TaskState.Running:
+                    icon.Icon = FluentSystemIcons.Play_20_Regular;
+                    break;
+                case TaskState.Completed:
+                    icon.Icon = FluentSystemIcons.Check_20_Regular;
+                    break;
+                case TaskState.Faulted:
+                    icon.Icon = FluentSystemIcons.ErrorCircle_20_Regular;
+                    break;
+                case TaskState.Cancelled:
+                    icon.Icon = FluentSystemIcons.Delete_20_Regular;
+                    break;
+            }
+        }
+    }
+}
